98458d1aacc4040856045bdf8fc6c68087a0f22d
kevinmitch14 <55989505+kevinmitch14@users.noreply.github.com>
2020-04-10 23:10:38 +0100

    Updated Connection problems

--- app/main.py
+++ app/main.py
@@ -1,10 +1,13 @@
 from flask import Flask, render_template, g, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
+from sqlalchemy.pool import NullPool
 import pymysql
 import urllib.request
 import simplejson
 import json
+import pickle
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -22,6 +25,7 @@ def connectDB():
         print("Error:", type(e))
         print(e)
 
+engine = connectDB()
 
 @app.route("/")
 def home():
@@ -32,17 +36,18 @@ def home():
 
 @app.route("/stations")
 def get_stations():
-    engine = connectDB()
+    #engine = connectDB()
     stations = []
     conn=engine.connect()
     rows = conn.execute("SELECT * from StaticData order by number;")
     for row in rows:
         stations.append(dict(row))
+    conn.close()
     return jsonify(stations)
 
 @app.route("/occupancy")
 def get_occupancy():
-    engine = connectDB()
+    #engine = connectDB()
     occupancyData = []
     conn = engine.connect()
     sql = """SELECT distinct number, available_bikes, available_bike_stands, total_stands, name, latitude, longitude, banking, last_update FROM (
@@ -51,34 +56,37 @@ def get_occupancy():
     data = conn.execute(sql)
     for row in data:
         occupancyData.append(dict(row))
+    conn.close()
     return jsonify(occupancyData)
 
 @app.route("/weather")
 def getWeather():
-    engine = connectDB()
+    #engine = connectDB()
     weather = []
     conn = engine.connect()
     rows = conn.execute("SELECT * FROM DynamicData.weatherData order by DATE desc, time desc limit 1 ;")
     for row in rows:
         weather.append(dict(row))
+    conn.close()
     return jsonify(weather)
 
 
 @app.route("/dynamicData/<station_id>")
 def get_dynamic_data(station_id):
-    engine = connectDB()
+    #engine = connectDB()
     stationData = []
     conn=engine.connect()
     sql = "SELECT * FROM DynamicData where number = " + station_id + " order by last_update DESC limit 1;"
     rows = conn.execute(sql)
     for row in rows:
         stationData.append(dict(row))
+    conn.close()
     return jsonify(stationData)
 
 
 @app.route("/dailyData/<station>")
 def get_day_data(station):
-    engine = connectDB()
+    #engine = connectDB()
     daily = []
     conn = engine.connect()
 
@@ -87,12 +95,13 @@ def get_day_data(station):
         dailydata = conn.execute(sql)
         for row in dailydata:
             daily.append(dict(row))
+    conn.close()
     return jsonify(daily)
 
 
 @app.route("/hourlyData/<station>/<day>")
 def get_hourly_data(station, day):
-    engine = connectDB()
+    #engine = connectDB()
     hourly = []
     conn = engine.connect()
 
@@ -101,8 +110,52 @@ def get_hourly_data(station, day):
         hourlydata = conn.execute(sql)
         for row in hourlydata:
             hourly.append(dict(row))
+    conn.close()
     return jsonify(hourly)
 
+
+@app.route("/predict/<station>/<day>/<hour>/<totalStands>")
+def prediction(station, day, hour, totalStands):
+
+    def setValues(df, dict):
+        df['number'][0] = dict['number']
+        df['total_stands'][0] = dict['total_stands']
+        df['day_' + dict['day']][0] = 1
+        df['hour_' + dict['hour']][0] = 1
+
+    def initDF():
+    
+        hours = [0] * 19
+    
+        days = [0] * 7
+    
+        number = [0] * 1
+    
+        stands = [0] * 1
+    
+        data = [number + stands + days + hours]
+    
+        df = pd.DataFrame(data, columns=['number', 'total_stands', 'day_0', 'day_1', 'day_2', 'day_3', 'day_4', 'day_5', 'day_6', 'hour_5', 'hour_6', 'hour_7', 'hour_8', 'hour_9',
+                                  'hour_10', 'hour_11', 'hour_12', 'hour_13', 'hour_14', 'hour_15', 'hour_16', 'hour_17',
+                                  'hour_18', 'hour_19', 'hour_20', 'hour_21', 'hour_22', 'hour_23'])
+    
+        return df
+
+    inputs = {}
+
+    inputs['number'] = station
+    inputs['total_stands'] = totalStands
+    inputs['day'] = day
+    inputs['hour'] = hour
+
+    X = initDF()
+
+    setValues(X, inputs)
+
+    model = pickle.load(open('app/static/models/model.pkl','rb'))
+    prediction = model.predict(X)
+
+    return jsonify(availability=int(round(prediction[0])))
 
 
 if __name__ == "__main__":
